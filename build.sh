ad3fb5164047a6ab9153d7c9387e18ea4bb91772
Lemon <lemon@linux.do>
2024-09-25 13:55:44 +0800

    feat: 更新 build.sh 脚本以自动更新 .env 文件中的版本信息
    实现获取最新 Git 标签并更新 .env 文件中的 VERSION 值，支持 macOS 和 Linux 系统。添加对不支持的操作系统的提示。调整环境变量的导入顺序。

--- build.sh
+++ build.sh
@@ -3,13 +3,42 @@
 # 记录构建开始时间
 start_time=$(date +%s)
 
-# 从 .env 文件中导入环境变量
+# 获取最新的标签名
+TAG=$(git describe --tags --abbrev=0)
+
+# 检查是否获取到标签名
+if [ -z "$TAG" ]; then
+  echo "No Git tags found."
+  exit 1
+fi
+
+# 转义标签名中的特殊字符
+ESCAPED_TAG=$(printf '%s\n' "$TAG" | sed 's/[\/&]/\\&/g')
+
+# 获取操作系统名称
+OS_NAME=$(uname)
+
+# 更新 .env 文件中的 VERSION 值
 if [ -f ".env" ]; then
-    export $(cat .env | sed 's/#.*//g' | xargs)
+
+    if [ "$OS_NAME" = "Darwin" ]; then
+        # macOS 使用 BSD sed
+        sed -i '' "s/^VERSION=.*/VERSION=$ESCAPED_TAG/" .env
+    elif [ "$OS_NAME" = "Linux" ]; then
+        # Linux 使用 GNU sed
+        sed -i "s/^VERSION=.*/VERSION=$ESCAPED_TAG/" .env
+    else
+        # 其他操作系统，提示不支持
+        echo "Unsupported operating system: $OS_NAME"
+        exit 1
+    fi
 else
     echo ".env file not found"
     exit 1
 fi
+
+# 从 .env 文件中导入环境变量
+export $(cat .env | sed 's/#.*//g' | xargs)
 
 # 使用环境变量中的用户名和密码尝试登录Docker Hub
 docker login -u="${HUB_USER}" -p="${HUB_PASS}"
@@ -52,14 +81,18 @@ end_time=$(date +%s)
 elapsed_time=$(( end_time - start_time ))
 
 # 判断运行的操作系统，分别采用兼容的日期命令
-if [[ "$(uname)" == "Darwin" ]]; then
+if [ "$OS_NAME" = "Darwin" ]; then
     # Mac OS 使用的date命令
     start_fmt=$(date -r $start_time '+%Y-%m-%d %H:%M:%S')
     end_fmt=$(date -r $end_time '+%Y-%m-%d %H:%M:%S')
-else
+elif [ "$OS_NAME" = "Linux" ]; then
     # Linux 使用的date命令
     start_fmt=$(date -d @$start_time '+%Y-%m-%d %H:%M:%S')
     end_fmt=$(date -d @$end_time '+%Y-%m-%d %H:%M:%S')
+else
+    # 其他操作系统，提示不支持
+    echo "Unsupported operating system: $OS_NAME"
+    exit 1
 fi
 
 echo "Build started at: $start_fmt"
